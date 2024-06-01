5c0827b53d937de18d9fd8f50ef298d5ba87d033
Yifan Peng <yifanp@nvidia.com>
2024-05-31 20:59:29 -0700

    fix question

--- nemo/collections/multimodal/speech_llm/data/lhotse_dataset.py
+++ nemo/collections/multimodal/speech_llm/data/lhotse_dataset.py
@@ -364,19 +364,18 @@ class LhotseAudioQuestionAnswerDataset(torch.utils.data.Dataset):
                 else:
                     audio_ratio.append(1.0)
 
-            if self.canary_processor != None:
+            if self.canary_processor is not None:
                 is_canary_tokens_augment = torch.rand(1) < self.canary_tokens_augment_ratio
                 _, _, _, _, canary_tokens, canary_token_lens = self.canary_processor.__getitem__(cuts)
                 for id, cut in enumerate(cuts):
                     canary_text = self.canary_processor.tokenizer._tokenizer.ids_to_text(canary_tokens[id].tolist())
                     if audio_ratio[id] == 0.0:
                         assert hasattr(cut, "question")
-                    elif self.prepend_to_exist_question and hasattr(cut, "question"):
-                        cut.question = self.prepend_to_exist_question + cut.question
+                    elif hasattr(cut, "question"):
+                        if self.prepend_to_exist_question:
+                            cut.question = self.prepend_to_exist_question + cut.question
                     elif self.convert_canary_prompt_to_text:
                         cut.question = convert_canary_prompt_to_text(canary_text, is_canary_tokens_augment)
-                    elif hasattr(cut, "question"):
-                        pass
                     else:
                         cut.question = self.question + ' ' + canary_text
             metadata = []
