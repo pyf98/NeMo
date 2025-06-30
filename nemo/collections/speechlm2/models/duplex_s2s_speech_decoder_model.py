69d13976760eba192e0d8e6adc004a00723adb32
Edresson Casanova <edresson1@gmail.com>
2025-06-30 13:55:56 -0700

    Add custom_nonsil_bos_eos

--- nemo/collections/speechlm2/models/duplex_s2s_speech_decoder_model.py
+++ nemo/collections/speechlm2/models/duplex_s2s_speech_decoder_model.py
@@ -786,6 +786,13 @@ class DuplexS2SSpeechDecoderModel(LightningModule, HFHubMixin):
         if self.cfg.get("scale_loss_by", None):
             if self.cfg.scale_loss_by == 'non_sil_t':
                 loss_scale[:, :, :1] = torch.where(text_labels.unsqueeze(-1) != self.text_pad_id, 4.0, loss_scale[:, :, :1])
+            elif self.cfg.scale_loss_by == 'custom_nonsil_bos_eos':
+                loss_scale[:, :, :1] = torch.where(text_labels.unsqueeze(-1) != self.text_pad_id, self.cfg.get("nonsil_weight", 1.0),
+                                                   loss_scale[:, :, :1])
+                loss_scale[:, :, :1] = torch.where(text_labels.unsqueeze(-1) == self.text_bos_id, self.cfg.get("bos_weight", 10.0),
+                                                   loss_scale[:, :, :1])
+                loss_scale[:, :, :1] = torch.where(text_labels.unsqueeze(-1) == self.text_eos_id, self.cfg.get("eos_weight", 10.0),
+                                                   loss_scale[:, :, :1])
             elif self.cfg.scale_loss_by == 'dynamic_text_non_sil_4x_and_bos_eos':
                 # Set text loss weights
                 for i in range(text_labels.size(0)):
